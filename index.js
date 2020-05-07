20b524e9d267b0a2049e116796d0880a09594835
LooDaHu <651120561@qq.com>
2020-05-07 10:13:55 -0700

    add username and commit num API
    Signed-off-by: LooDaHu <651120561@qq.com>

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ const github = require('@actions/github');
 const shell = require('shelljs');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    // const nameToGreet = core.getInput('who-to-greet');
-    const nameToGreet = "Jinming Yang"
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+    const commit = core.getInput('commit');
+    const username = core.getInput('username');
     installDependencies();
     installR();
-    Rperform();
-    updateResult();
+    Rperform(commit);
+    updateResult(username);
 } catch (error) {
     core.setFailed(error.message);
 }
@@ -33,13 +26,17 @@ function installR() {
     shell.exec('sudo R --version');
 }
 
-function Rperform() {
-  shell.exec('sudo R -e \'install.packages("devtools");devtools::install_github("LooDaHu/Rperform");devtools::install_dev_deps();Rperform::run_all_test(num_commits=1);q()\'');
+function Rperform(commitNum) {
+  shell.exec('sudo R -e \'install.packages("devtools");' +
+      'devtools::install_github("LooDaHu/Rperform");' +
+      'devtools::install_dev_deps();' +
+      'Rperform::run_all_test(num_commits='+ commitNum+');' +
+      'q()\'');
 }
 
-function updateResult() {
-  shell.exec('git config --global user.name \'LooDaHu\'');
-  shell.exec('git config --global user.email \'LooDaHu@users.noreply.github.com\'');
+function updateResult(username) {
+  shell.exec('git config --global user.name\''+ username +'\'');
+  shell.exec('git config --global user.email \''+ username + '@users.noreply.github.com\'');
   shell.exec('git add Rperform_Data/');
   shell.exec('git commit -m "Benchmark Result Update"');
   shell.exec('git push');
